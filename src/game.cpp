329570ea69d1638f97b03ae0b3e5b4e46f24c9b0
noche <micheal4826@tuta.io>
2021-12-08 00:26:24 +0300

    Add windows offsets
    Signed-off-by: noche <micheal4826@tuta.io>

--- src/game.cpp
+++ src/game.cpp
@@ -1,12 +1,19 @@
 #include "game.hpp"
+#include "structs.hpp"
 
 game::game(std::uintptr_t base) {
-    drawTextShadow = base + 0x2c62c;
-    drawTextShadowFunc = (decltype(drawTextShadowFunc))(drawTextShadow);
+    drawText = base + WIN_LIN(0x6d930, 0x2c62c);
+    drawTextFunc = (decltype(drawTextFunc))(drawText);
     
-    renderFrame = base + 0x127844;
+    renderFrame = base + WIN_LIN(0x11fd00, 0x127844);
     renderFrameFunc = (decltype(renderFrameFunc))(renderFrame);
 
-    drawHud = base + 0x5bfed;
+    drawHud = base + WIN_LIN(0x86000, 0x5bfed);
     drawHudFunc = (decltype(drawHudFunc))(drawHud);
+
+    drawMainMenu = base + WIN_LIN(0xfe5d0, 0x8d9c2);
+    drawMainMenuFunc = (decltype(drawMainMenuFunc))(drawMainMenu);
+
+    drawCreditsMenu = base + WIN_LIN(0xfcc90, 0x8d1ed);
+    drawCreditsMenuFunc = (decltype(drawCreditsMenuFunc))(drawCreditsMenu);
 }
