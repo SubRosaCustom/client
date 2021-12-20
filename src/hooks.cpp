801d256be7c14cab587db8c536315d34be8b3ce6
checkraisefold <devsmash1@gmail.com>
2021-12-20 00:43:06 -0800

    Linux fix

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -51,7 +51,7 @@ int64_t drawHud(int64_t arg1)
 #ifdef _WIN32
 int64_t drawText(char *text, float x, float y, float scale, int params, float red, float green, float blue, float alpha, ...)
 #else
-int64_t drawText(char *text, int params, int a, int b, float x, float y, float scale, float red, float green, float blue, float alpha, int c)
+int64_t drawText(char *text, int params, float x, float y, float scale, float red, float green, float blue, float alpha, ...)
 #endif
 {
 	REMOVE_HOOK(drawText);
@@ -66,7 +66,14 @@ int64_t drawText(char *text, int params, int a, int b, float x, float y, float s
 	if (argCount > 0)
 		clearStack(static_cast<long long>(argCount));
 #else
-	auto ret = g_game->drawTextFunc(text, params | TEXT_SHADOW, a, b, x, y, scale, red, green, blue, alpha, c);
+	std::string_view textStr = text;
+	auto argCount = std::count(textStr.begin(), textStr.end(), '/'); // this will break if alex uses a / escape or some shit (lol)
+	if (argCount > 0) {
+		pushVarArgs(&alpha, static_cast<long long>(argCount));
+	}
+	auto ret = g_game->drawTextFunc(text, params | TEXT_SHADOW, x, y, scale, red, green, blue, alpha);
+	if (argCount > 0)
+		clearStack(static_cast<long long>(argCount));
 #endif
 	// printf("DrawText %s, %#x, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %i\n", text, params, a, b, x, y, scale, red, green, blue, alpha, c);
 	return ret;
