52e6600a043523967e742a8708dc239cb08b1f85
checkraisefold <devsmash1@gmail.com>
2021-12-21 01:30:21 -0800

    Increase text size limit at almost no cost ref. https://www.cplusplus.com/reference/cstdio/vsnprintf/ for size not having to be 1024-1

--- src/api.cpp
+++ src/api.cpp
@@ -7,10 +7,10 @@ namespace api
 {
     void drawText(const char *text, float x, float y, float scale, int params, float r, float g, float b, float a, ...)
     {
-        char buffer[256];
+        char buffer[1024];
         va_list list;
         va_start(list, a);
-        vsnprintf(buffer, 255, text, list);
+        vsnprintf(buffer, 1024, text, list);
 #ifdef _WIN32
         g_game->drawTextFunc(buffer, x, y, scale, params, r, g, b, a);
 #else
